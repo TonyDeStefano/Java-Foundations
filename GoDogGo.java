35bc086c2ed4352f93804d9c08477b1e5a369a96
Tony DeStefano <tdestefano@funko.com>
2019-08-14 20:26:36 -0700

    Variables and printing.

---
+++ GoDogGo.java
@@ -0,0 +1,22 @@
+public class GoDogGo {
+
+	public static void main(String[] args)
+	{
+		int daysInAugust = 31;
+		char firstInitial = 'T';
+		double hourlyRate = 99.99;
+
+		System.out.println(daysInAugust);
+		System.out.println(firstInitial);
+		System.out.println(hourlyRate);
+		System.out.println("*********");
+		System.out.println(" *******");
+		System.out.println("  *****");
+		System.out.println("   ***");
+		System.out.println("    *");
+		System.out.println("   ***");
+		System.out.println("  *****");
+		System.out.println(" *******");
+		System.out.println("*********");
+	}
+}
